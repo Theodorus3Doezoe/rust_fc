9a23b2326778bf05e0d109bbccd35d8ff19a7c4b
i569189 <i569189@student.fontys.nl>
2026-08-14 22:14:03 +0200

    Add MPU6500 register config

--- src/sensors/mpu6500.rs
+++ src/sensors/mpu6500.rs
@@ -1,10 +1,28 @@
 use super::{Imu, ImuBurst, Vector3D};
 use embedded_hal_async::spi::{Operation, SpiDevice};
 
+// Data read start registers & scale factors
 const ACCEL_START: u8 = 0x3B | 0x80;
 const GYRO_START: u8 = 0x43 | 0x80;
-const ACCEL_SCALE: f32 = 8192.0;
-const GYRO_SCALE: f32 = 65.5;
+const ACCEL_SCALE: f32 = 8192.0; // ±4g range
+const GYRO_SCALE: f32 = 65.5; // ±500 °/s range
+
+// Configuration registers
+const SMPLRT_DIV: u8 = 0x19;
+const CONFIG: u8 = 0x1A;
+const GYRO_CONFIG: u8 = 0x1B;
+const ACCEL_CONFIG: u8 = 0x1C;
+const ACCEL_CONFIG_2: u8 = 0x1D;
+const USER_CTRL: u8 = 0x6A;
+const PWR_MGMT_1: u8 = 0x6B;
+
+// Configuration values and bitmasks
+const GYRO_FS_500: u8 = 0x08;
+const ACCEL_FS_4G: u8 = 0x08;
+const GYRO_BYPASS: u8 = 0x03; // FCHOICE_B = 11 (bypasses internal gyro DLPF)
+const ACCEL_BYPASS: u8 = 0x08; // ACCEL_FCHOICE_B = 1 (bypasses internal accel DLPF)
+const PWR_WAKE_AUTO_CLK: u8 = 0x01; // CLKSEL = 1 (auto-selects best available gyro PLL clock)
+const I2C_DISABLE: u8 = 0x10; // I2C_IF_DIS = 1 (disables I2C to prevent bus glitches)
 
 pub struct Mpu6500<SPI> {
     spi: SPI,
@@ -26,11 +44,17 @@ impl<SPI: SpiDevice> Mpu6500<SPI> {
             who_am_i
         );
 
-        // TODO
-        // Write register config
-        // Run mpu at 8mhz
-        // use the mpu6500 interrupts pin instead of ticker later
-        // Set spi speed at 20mhz after writing registers
+        spi.write(&[PWR_MGMT_1, PWR_WAKE_AUTO_CLK]).await?;
+        embassy_time::Timer::after_millis(100).await;
+        spi.write(&[USER_CTRL, I2C_DISABLE]).await?;
+
+        spi.write(&[SMPLRT_DIV, 0x00]).await?;
+        spi.write(&[CONFIG, 0x00]).await?;
+        spi.write(&[GYRO_CONFIG, GYRO_FS_500 | GYRO_BYPASS]).await?;
+        spi.write(&[ACCEL_CONFIG, ACCEL_FS_4G]).await?;
+        spi.write(&[ACCEL_CONFIG_2, ACCEL_BYPASS]).await?;
+
+        defmt::info!("MPU6500 initalized");
 
         Ok(Self { spi })
     }
